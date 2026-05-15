232a71e5667d9997d5fc093854e788c03eccb3b5
Jay Tapodhan <jaytapodhan21@gmail.com>
2026-05-15 14:42:40 +0530

    Update portfolio.ts

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -112,7 +112,7 @@ const portfolio: PortfolioData = {
       { icon: 'download', text: 'Resume',   url: '/Jay_Tapodhan_Resume.pdf', modal: true },
     ],
   },
-  footer: { name: 'JAY TAPODHAN', role: 'COMPUTER ENGINEERING', year: '2025' },
+  footer: { name: 'JAY TAPODHAN', role: 'COMPUTER ENGINEERING'},
 };
 
 export default portfolio;
